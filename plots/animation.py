a1bb6384933d6171400acb0afdf7e338bbcac5d2
Luca Nigro <luca.nigro@studenti.unimi.it>
2022-12-10 23:55:43 +0100

    Fix animation.py

--- plots/animation.py
+++ plots/animation.py
@@ -21,23 +21,17 @@ p3 = pa.cube()
 p3.load('../build/data/modulus_nslit3.bin')  # triple-slit
 p3 = np.array(p3)
 
-# Set up a 2D xy grid
-h = 0.005
-x_points = np.arange(0, 1+h, h)
-y_points = np.arange(0, 1+h, h)
-x, y = np.meshgrid(x_points, y_points, sparse=True)
-
-# Array of time points
 dt = 0.000025
 t_points = np.arange(0, 1+dt, dt)
+t_min = t_points[0]
+x_min, x_max = 0, 1
+y_min, y_max = 0, 1
 
 # Some settings
 fontsize = 12
 w = 10
 h = 10
-t_min = t_points[0]
-x_min, x_max = x_points[0], x_points[-1]
-y_min, y_max = y_points[0], y_points[-1]
+show_colorbar = False
 
 nslit = 0
 plist = [p0,p1,p2,p3]
@@ -46,32 +40,32 @@ for p in plist:
     fig = plt.figure(figsize=(w,h))
     ax = plt.gca()
     
-    # Create a colour scale normalization according to the max z value in the first frame
+    # Create a colour scale normalization according to the max value in the frame
     norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=np.max(p[0]))
 
     # Plot the first frame
-    img = ax.imshow(p[0], extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("inferno"), norm=norm, interpolation='Gaussian')
+    img = ax.imshow(p[0].T, extent=[x_min,x_max,y_min,y_max], cmap=plt.get_cmap("inferno"), norm=norm, interpolation='Gaussian')
 
     # Axis labels
     plt.axis('off')
 
-    # Add a colourbar
-    cbar = fig.colorbar(img, ax=ax)
-    cbar.set_label("probability", fontsize=fontsize)
-    cbar.ax.tick_params(labelsize=fontsize)
+    # Add a colorbar
+    if show_colorbar:
+        cbar = fig.colorbar(img, ax=ax)
+        cbar.set_label("probability", fontsize=fontsize)
+        cbar.ax.tick_params(labelsize=fontsize)
 
     # Add a text element showing the time
     time_txt = plt.text(0.95, 0.95, "t = {:.3e}".format(t_min), color="white", 
                         horizontalalignment="right", verticalalignment="top", fontsize=fontsize)
 
-    # Function that takes care of updating the z data and other things for each frame
+    # Function that takes care of updating the data and other things for each frame
     def animation(i):
         # Normalize the colour scale to the current frame?
         norm = matplotlib.cm.colors.Normalize(vmin=0.0, vmax=np.max(p[i]))
         img.set_norm(norm)
 
-        # Update z data
-        img.set_data(p[i])
+        img.set_data(p[i].T)
 
         # Update the time label
         current_time = t_min + i * dt
